caf06b896cc5c0953086b34dceb01c49622507e7
danieljaller <danieljaller@gmail.com>
2017-05-08 10:23:47 +0200

    Added some props for samurai

---
+++ EFSamurai.Data/Migrations/20170508081602_Second.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFSamurai.Data.Migrations
+{
+    public partial class Second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "Samurais",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "HasKatana",
+                table: "Samurais",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Samurais",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "Samurais");
+
+            migrationBuilder.DropColumn(
+                name: "HasKatana",
+                table: "Samurais");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Samurais");
+        }
+    }
+}
--- EFSamurai.Domain/Samurai.cs
+++ EFSamurai.Domain/Samurai.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace EFSamurai.Domain
 {
     public class Samurai
     {
         public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public bool HasKatana { get; set; }
     }
 }
